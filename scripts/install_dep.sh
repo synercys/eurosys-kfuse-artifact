b21874737daacc8426dfe707276dcc0c954cccf0
hckuo <hckuo2@illinois.edu>
2022-01-23 16:08:22 -0600

    add deps

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install iptables iproute2 perl git qemu-system-x86
+apt update
+apt install iptables iproute2 perl git qemu-system-x86
 
-git clone --depth=1 git@github.com:nferraz/st.git
+git clone --depth=1 https://github.com/nferraz/st.git
 cd st
 perl Makefile.PL
 make
-sudo make install
+make install
 cd ..
