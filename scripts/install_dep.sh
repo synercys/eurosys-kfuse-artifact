087748327f6659a4fc661a5884e82345bc1ea1b5
Kai-Hsun Chen <kaihsun@apache.org>
2022-01-24 11:57:08 -0600

    update install_dep.sh

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -3,6 +3,15 @@
 apt update
 apt install --no-install-recommends iptables iproute2 perl git qemu-system-x86 make
 
+apt-get update
+apt-get install build-essential autoconf automake libpcre3-dev libevent-dev pkg-config zlib1g-dev libssl-dev libtool
+git clone https://github.com/RedisLabs/memtier_benchmark.git
+cd memtier_benchmark
+autoreconf -ivf
+./configure
+make
+make install
+
 git clone --depth=1 https://github.com/nferraz/st.git
 cd st
 perl Makefile.PL
